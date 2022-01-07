0b2940dae84553d7ad3ab4ca307893054456a6ee
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-01-07 16:44:07 +0200

    Added templates, static files and views

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join('static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ mysite/mysite/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def dfs_home(request):
+    return render(request, 'dfs_home.html')
+
+
+def kmeans_home(request):
+    return render(request, 'kmeans_home.html')
+
+
+def knn_home(request):
+    return render(request, 'knn_home.html')
+
+
+def lda_home(request):
+    return render(request, 'lda_home.html')
+
+
+def mle_home(request):
+    return render(request, 'mle_home.html')
+
+
+def naive_bayes_home(request):
+    return render(request, 'naive_bayes_home.html')
+
+
+def pca_home(request):
+    return render(request, 'pca_home.html')
+
+
+def svm_home(request):
+    return render(request, 'svm_home')
