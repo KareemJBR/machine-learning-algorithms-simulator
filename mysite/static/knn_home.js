756f0d3226fa0403a364cc253c200a3c8cca3479
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-01-18 17:14:30 +0200

    Added github references and set up the js file for knn

--- mysite/static/knn_home.js
+++ mysite/static/knn_home.js
@@ -1,1 +1,26 @@
 let k = 1;
+let num_classes = 4, num_points = 40;
+
+const MAX_CLASSES = 6, MIN_CLASSES = 1;
+const MAX_POINTS = 100, MIN_POINTS = 10;
+
+const classes_colors = [
+    'red',
+    'green',
+    'blue',
+    'purple',
+    'orange',
+    'brown'
+]
+
+function main() {
+    let HEIGHT = 600, WIDTH = 800;
+    let canvas = document.getElementById('canvas')
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+}
+
+
+function gen_points() {
+
+}
