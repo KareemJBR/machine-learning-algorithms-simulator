87ac68da68f806fcd723dc64e5febc3b1386b3fb
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-02-24 16:53:23 +0200

    Initial implementation for SVM

---
+++ mysite/mysite/support_vectors_machines.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+class SVM:
+    def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iterations=1000):
+        self.learning_rate = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iterations = n_iterations
+        self.w = None
+        self.b = None
+
+    def fit(self, data, y):
+        n_samples, n_features = data.shape
+
+        y_ = np.where(y <= 0, -1, 1)
+
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+        for _ in range(self.n_iterations):
+            for idx, x_i in enumerate(data):
+                condition = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
+                if condition:
+                    self.w -= self.learning_rate * (2 * self.lambda_param * self.w)
+                else:
+                    self.w -= self.learning_rate * (
+                        2 * self.lambda_param * self.w - np.dot(x_i, y_[idx])
+                    )
+                    self.b -= self.learning_rate * y_[idx]
+
+    def predict(self, x):
+        approx = np.dot(x, self.w) - self.b
+        return np.sign(approx)
