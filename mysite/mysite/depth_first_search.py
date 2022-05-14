111cd3b4ef0b214dbf5bb965d0dd9dfdea4d97e4
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-15 01:11:19 +0300

    Removed irrelevant code

--- mysite/mysite/depth_first_search.py
+++ mysite/mysite/depth_first_search.py
@@ -1,19 +1,3 @@
-class Edge:
-
-    def __init__(self, edge_id: int, src_id, dist_id):
-        self.edge_id = edge_id
-        self.src_id = src_id
-        self.dist_id = dist_id
-
-
-class Node:
-
-    def __init__(self, node_id: int, in_edges: list, out_edges: list):
-        self.node_id = node_id
-        self.in_edges = in_edges
-        self.out_edges = out_edges
-
-
 def dfs(visited_nodes, node):
     if node.node_id not in visited_nodes:
         visited_nodes.append(node)
