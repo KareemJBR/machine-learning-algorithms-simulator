c16625f932bebf3e4b53962d405a6df8677dff5e
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-05-18 14:51:43 +0300

    Improving time complexity

--- mysite/mysite/help_functions.py
+++ mysite/mysite/help_functions.py
@@ -1,5 +1,6 @@
-import numpy as np
+from numpy import sqrt as sq
+from numpy import sum as su
 
 
 def euclidean_distance(x1, x2):
-    return np.sqrt(np.sum((x1 - x2) ** 2))
+    return sq(su((x1 - x2) ** 2))
