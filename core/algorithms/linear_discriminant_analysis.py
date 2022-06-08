65ee10eaa7d8ae56c196f00f43fad2236188ff05
Kareem <77075186+KareemJBR@users.noreply.github.com>
2022-06-09 00:02:00 +0300

    finished LDA Python code

--- core/algorithms/linear_discriminant_analysis.py
+++ core/algorithms/linear_discriminant_analysis.py
@@ -2,39 +2,33 @@ import numpy as np
 
 
 class LDA:
-    def __init__(self, n_components):
+    def __init__(self, n_components=None):
         self.n_components = n_components
-        self.linear_discriminants = None
+        self.eig_vectors = None
 
-    def fit(self, x, y):
-        n_features = x.shape[1]
-        class_labels = np.unique(y)
+    def transform(self, x, y):
+        height, width = x.shape
+        unique_classes = np.unique(y)
+        num_classes = len(unique_classes)
 
-        mean_overall = np.mean(x, axis=0)
-        s_w = np.zeros((n_features, n_features))
-        s_b = np.zeros((n_features, n_features))
-        for c in class_labels:
-            x_c = x[y == c]
-            mean_c = np.mean(x_c, axis=0)
-            # (4, n_c) * (n_c, 4) = (4,4) -> transpose
-            s_w += (x_c - mean_c).T.dot((x_c - mean_c))
+        scatter_t = np.cov(x.T) * (height - 1)
+        scatter_w = 0
+        for i in range(num_classes):
+            class_items = np.flatnonzero(y == unique_classes[i])
+            scatter_w = scatter_w + np.cov(x[class_items].T) * (len(class_items) - 1)
 
-            # (4, 1) * (1, 4) = (4,4) -> reshape
-            n_c = x_c.shape[0]
-            mean_diff = (mean_c - mean_overall).reshape(n_features, 1)
-            s_b += n_c * mean_diff.dot(mean_diff.T)
-
-        a = np.linalg.inv(s_w).dot(s_b)
-        # get eigenvalues and eigenvectors of s_w^-1 * s_b
-        eigenvalues, eigenvectors = np.linalg.eig(a)
-        # eigenvector v = [:,i] column vector, transpose for easier calculations
-        # sort eigenvalues high to low
-        eigenvectors = eigenvectors.T
-        idxs = np.argsort(abs(eigenvalues))[::-1]
-        eigenvectors = eigenvectors[idxs]
-        # store first n eigenvectors
-        self.linear_discriminants = eigenvectors[0: self.n_components]
-
-    def transform(self, x):
-        # project data
-        return np.dot(x, self.linear_discriminants.T)
+        scatter_b = scatter_t - scatter_w
+        _, eig_vectors = np.linalg.eigh(np.linalg.pinv(scatter_w).dot(scatter_b))
+        pc = x.dot(eig_vectors[:, ::-1][:, : self.n_components])
+        response = []
+        if self.n_components == 2:
+            labels = np.unique(y)
+            for label in labels:
+                class_data = pc[np.flatnonzero(y == label)]
+                item = {
+                    "x": class_data[:, 0].tolist(),
+                    "y": class_data[:, 1].tolist(),
+                    "label": int(label),
+                }
+                response.append(item)
+        return response
